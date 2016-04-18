d64adb2aa3d0b65d0bacc9dca917f46a2e377851
Alexander Scholz <alex@nodev.de>
2016-04-18 21:32:54 +0200

    nussinov rewritten as class, modified and added nussinov tests, resource file for coverage

--- semester2/rna_structures/nussinov.py
+++ semester2/rna_structures/nussinov.py
@@ -21,165 +21,165 @@ from copy import deepcopy
 #                 ---
 ###
 
-# watson-crick and wobble base pairs
-allowed_base_pairs = ['AU', 'UA', 'GC', 'CG', 'GU', 'UG']
+class Nussinov():
 
-# sequence = 'AGGCAAUGCC'
-# sequence = 'GGGAAAUCC'
-sequence = 'GGCAGACUAU'
-# sequence = "GACUCCGUGGCGCAACGGUAGCGCGUC"\
-# "CGACUCCAGAUCGGAAGGUUGCGUGUUCAAAUCACGUCGGGGUCA"
+    # watson-crick and wobble base pairs
+    _allowed_base_pairs = ['AU', 'UA', 'GC', 'CG', 'GU', 'UG']
 
-sequence_len = len(sequence)
+    _sequence = ''
+    _sequence_len = 0
+    _result_sequence = None
 
-# minimal count of base between
-min_count = 3
-# min_count = 1
+    # minimal count of base between
+    _min_count = 3
 
-# create empty matrix
-matrix = [['']*sequence_len for i in range(sequence_len)]
+    # create empty matrix
+    _matrix = None
 
-result_sequence = [''] * sequence_len
+    def __init__(self, sequence):
+        self._sequence = sequence
+        self._sequence_len = len(sequence)
+        self._result_sequence = [''] * self._sequence_len
+        self._matrix = [[''] * self._sequence_len for i in range(self._sequence_len)]
 
+    # initialization
+    def initiate(self):
 
-# initialization
-def init():
-
-    # diagonal initialization
-    # initialization of i<j<=i+3
-    i = 0
-    for row in matrix:
-        for n in range(min_count+1):
-            if (i+n) < sequence_len:
-                row[i+n] = 0
-        i += 1
-
-    # additional initialization of N(i+1,j)
-    for i in range(0, sequence_len):
-        if i < (sequence_len-1):
-            matrix[i+1][i] = 0
-
-
-def getK(i, j):
-    k = list()
-
-    for s in range(i, j+1):
-        if (s > i) & (s <= j):
-            k.append(s)
-
-    return k
-
-
-def getBasePairedValue(i, k):
-    base_pair = sequence[i] + sequence[k]
-    return 1 if base_pair in allowed_base_pairs else 0
-
-
-def calculate():
-    j_start = min_count + 1
-
-    s = 0
-    t = j_start
-    while t <= sequence_len:
+        # diagonal initialization, initialization of i<j<=i+3
         i = 0
-        jn = j_start + s
-        for j in range(jn, sequence_len):
-            k_values = getK(i+min_count, j)
-            results = []
-            for k in k_values:
-                trivial = matrix[i + 1][j]
-                results.append(trivial)
-
-                k_value = k + 1
-
-                base_paired_value = getBasePairedValue(i, k)
-                if k_value < sequence_len:
-                    calc = matrix[i+1][k-1] + matrix[k+1][j] +\
-                        base_paired_value
-                else:
-                    calc = matrix[i+1][k-1] + 0 + base_paired_value
-                results.append(calc)
-            matrix[i][j] = max(results)
+        for row in self._matrix:
+            for n in range(self._min_count+1):
+                if (i+n) < self._sequence_len:
+                    row[i+n] = 0
             i += 1
-        s += 1
-        t += 1
 
+        # additional initialization of N(i+1,j)
+        for i in range(0, self._sequence_len):
+            if i < (self._sequence_len-1):
+                self._matrix[i+1][i] = 0
 
-def output():
+    def _getK(self, i, j):
+        #return list(range(i+1, j+1))
+        k = list()
 
-    # copy matrix for output
-    output_matrix = deepcopy(matrix)
+        for s in range(i, j+1):
+            if (s > i) & (s <= j):
+                k.append(s)
 
-    # insert sequence as headline and headcolumn for view
-    output_matrix.insert(0, list(sequence))
+        return k
 
-    i = 0
-    for row in output_matrix:
-        if i == 0:
-            row.append('')
-        else:
-            row.append(sequence[i-1])
-        i += 1
+    def _getBasePairedValue(self, i, k):
+        base_pair = self._sequence[i] + self._sequence[k]
+        return 1 if base_pair in self._allowed_base_pairs else 0
 
-    # insert index numbers as headline and headcolumn for view
-    sequence_index = list(range(1, sequence_len+1))
-    output_matrix.insert(0, sequence_index)
+    def calculate(self):
+        j_start = self._min_count + 1
 
-    i = 0
-    for row in output_matrix:
-        if i < 2:
-            row.append('')
-        else:
-            row.append(i-1)
-        i += 1
+        s = 0
+        t = j_start
+        while t <= self._sequence_len:
+            i = 0
+            jn = j_start + s
+            for j in range(jn, self._sequence_len):
+                k_values = self._getK(i + self._min_count, j)
+                results = []
+                for k in k_values:
+                    trivial = self._matrix[i + 1][j]
+                    results.append(trivial)
 
-    # matrix output
-    for row in output_matrix:
-        print("\t".join(map(str, row)) + "\n")
-        pass
+                    k_value = k + 1
 
+                    base_paired_value = self._getBasePairedValue(i, k)
+                    if k_value < self._sequence_len:
+                        calc = self._matrix[i+1][k-1] + self._matrix[k+1][j] +\
+                            base_paired_value
+                    else:
+                        calc = self._matrix[i+1][k-1] + 0 + base_paired_value
+                    results.append(calc)
+                self._matrix[i][j] = max(results)
+                i += 1
+            s += 1
+            t += 1
 
-def backTracking():
+    def output(self):   # pragma: no cover (excluded from coverage)
 
-    j = sequence_len-1
-    for i in range(0, sequence_len-1):
-        if i <= (sequence_len / 2):
-            n_value = matrix[i][j]
+        # copy matrix for output
+        output_matrix = deepcopy(self._matrix)
 
-            # unpaired case
-            result = matrix[i+1][j]
+        # insert sequence as headline and headcolumn for view
+        output_matrix.insert(0, list(sequence))
 
-            if n_value == result:
-                result_sequence[i] = '.'
-                if i > 0:
-                    result_sequence[j] = '.'
+        i = 0
+        for row in output_matrix:
+            if i == 0:
+                row.append('')
+            else:
+                row.append(sequence[i-1])
+            i += 1
+
+        # insert index numbers as headline and headcolumn for view
+        sequence_index = list(range(1, self._sequence_len + 1))
+        output_matrix.insert(0, sequence_index)
+
+        i = 0
+        for row in output_matrix:
+            if i < 2:
+                row.append('')
+            else:
+                row.append(i-1)
+            i += 1
+
+        # matrix output
+        for row in output_matrix:
+            print("\t".join(map(str, row)) + "\n")
+
+    def backTracking(self):
+
+        j = self._sequence_len-1
+        for i in range(0, self._sequence_len-1):
+            if i <= (self._sequence_len / 2):
+                n_value = self._matrix[i][j]
+
+                # unpaired case
+                result = self._matrix[i+1][j]
+
+                if n_value == result:
+                    self._result_sequence[i] = '.'
+                    if i > 0:
+                        self._result_sequence[j] = '.'
+                        j -= 1
+                    # output_matrix[i][j+2] = '[' + output_matrix[i][j+2] + ']'
+                    continue
+
+                # paired cases
+                k_values = self._getK(i, j)
+                for k in k_values:
+                    f_value = self._getBasePairedValue(i, k)
+                    if f_value == 0:
+                        continue
+
+                    inner_value = self._matrix[i+1][k-1]
+                    outer_value = 0 if (k+1 > self._sequence_len-1) else self._matrix[k+1][j]
+                    result = f_value + inner_value + outer_value
+
+                    if result != n_value:
+                        continue
+
+                    self._result_sequence[i] = '('
+                    self._result_sequence[j] = ')'
                     j -= 1
-                # output_matrix[i][j+2] = '[' + output_matrix[i][j+2] + ']'
-                continue
-
-            # paired cases
-            k_values = getK(i, j)
-            for k in k_values:
-                f_value = getBasePairedValue(i, k)
-                if f_value == 0:
-                    continue
-
-                inner_value = matrix[i+1][k-1]
-                outer_value = 0 if (k+1 > sequence_len-1) else matrix[k+1][j]
-                result = f_value + inner_value + outer_value
-
-                if result != n_value:
-                    continue
-
-                result_sequence[i] = '('
-                result_sequence[j] = ')'
-                j -= 1
-                break
-    print(sequence)
-    print(''.join(result_sequence))
+                    break
+        print(sequence)
+        print(''.join(self._result_sequence))
 
 if __name__ == "__main__":
-    init()
-    calculate()
-    output()
-    backTracking()
+    # sequence = 'AGGCAAUGCC'
+    # sequence = 'GGGAAAUCC'
+    sequence = 'GGCAGACUAU'
+    # sequence = "GACUCCGUGGCGCAACGGUAGCGCGUC"\
+    # "CGACUCCAGAUCGGAAGGUUGCGUGUUCAAAUCACGUCGGGGUCA"
+    nussinov = Nussinov(sequence)
+    nussinov.initiate()
+    nussinov.calculate()
+    nussinov.output()
+    nussinov.backTracking()
--- test/test_semester2/test_rna_structures/nussinov_test.py
+++ test/test_semester2/test_rna_structures/nussinov_test.py
@@ -6,26 +6,79 @@ import sys
 script_path = os.path.abspath(__file__) + '/../../../..'
 sys.path.append(os.path.abspath(script_path))
 
-from semester2.rna_structures.nussinov import getK, getBasePairedValue, sequence
+from semester2.rna_structures.nussinov import Nussinov
 
 class TestNussinov(unittest.TestCase):
 
+    def testInit(self):
+        sequence = 'AGUC'
+        self.nussinov = Nussinov(sequence)
+        self.assertEqual(sequence, self.nussinov._sequence)
+        self.assertEqual(len(sequence), self.nussinov._sequence_len)
+
+        expected_result_sequence_init = [''] * len(sequence)
+        self.assertEqual(expected_result_sequence_init, self.nussinov._result_sequence)
+
+        matrix = [[''] * len(sequence) for i in range(len(sequence))]
+        self.assertEqual(matrix, self.nussinov._matrix)
+
+
+    def testInitiate(self):
+        sequence = 'AGUCGAUC'
+        self.nussinov = Nussinov(sequence)
+        self.nussinov.initiate()
+
+        expected = [
+            [0, 0, 0, 0, '', '', '', ''],
+            [0, 0, 0, 0, 0, '', '', ''],
+            ['', 0, 0, 0, 0, 0, '', ''],
+            ['', '', 0, 0, 0, 0, 0, ''],
+            ['', '', '', 0, 0, 0, 0, 0],
+            ['', '', '', '', 0, 0, 0, 0],
+            ['', '', '', '', '', 0, 0, 0],
+            ['', '', '', '', '', '', 0, 0]
+        ]
+        self.assertEqual(expected, self.nussinov._matrix)
+
     def testGetK(self):
 
+        self.nussinov = Nussinov('AGUC')
+
         expected = []
-        self.assertEqual(expected, getK(1, 1))
+        self.assertEqual(expected, self.nussinov._getK(1, 1))
 
         expected = [2, 3, 4]
-        self.assertEqual(expected, getK(1, 4))
+        self.assertEqual(expected, self.nussinov._getK(1, 4))
 
     def testGetBasePairedValue(self):
-        sequence = 'AGUC'
+
+        self.nussinov = Nussinov('AGUC')
+
         expected = 1
-        self.assertEqual(expected, getBasePairedValue(0, 2))
+        self.assertEqual(expected, self.nussinov._getBasePairedValue(0, 2))
 
         expected = 0
-        self.assertEqual(expected, getBasePairedValue(2, 3))
+        self.assertEqual(expected, self.nussinov._getBasePairedValue(2, 3))
 
+    def testCalculate(self):
+        sequence = 'GGCAGACUAU'
+        self.nussinov = Nussinov(sequence)
+        self.nussinov.initiate()
+        self.nussinov.calculate()
+
+        expected = [
+            [0, 0, 0, 0, 0, 0, 1, 2, 2, 2],
+            [0, 0, 0, 0, 0, 0, 1, 1, 1, 2],
+            ['', 0, 0, 0, 0, 0, 0, 1, 1, 1],
+            ['', '', 0, 0, 0, 0, 0, 1, 1, 1],
+            ['', '', '', 0, 0, 0, 0, 0, 0, 1],
+            ['', '', '', '', 0, 0, 0, 0, 0, 1],
+            ['', '', '', '', '', 0, 0, 0, 0, 0],
+            ['', '', '', '', '', '', 0, 0, 0, 0],
+            ['', '', '', '', '', '', '', 0, 0, 0],
+            ['', '', '', '', '', '', '', '', 0, 0]
+        ]
+        self.assertEqual(expected, self.nussinov._matrix)
 
 if __name__ == "__main__":
     unittest.main()
